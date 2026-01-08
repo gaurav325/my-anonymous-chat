90e2b46df42f2eb77c61b24656a86f1ab5c7034c
Gaurav kumar <114345408+gaurav325@users.noreply.github.com>
2026-01-08 19:11:10 +0530

    Simplify user management and chat message handling
    Refactor user handling and message emission logic.

--- server.js
+++ server.js
@@ -5,40 +5,25 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-// Online users ka data store karne ke liye
 let users = {};
 
 io.on('connection', (socket) => {
-    // Jab koi naya user login kare
     socket.on('login', (userData) => {
-        users[socket.id] = {
-            username: userData.username,
-            avatar: userData.avatar
-        };
-        // Sabko nayi user list bhejo
+        users[socket.id] = { username: userData.username, avatar: userData.avatar };
         io.emit('updateUserList', Object.values(users));
-        console.log(userData.username + " joined the lobby");
     });
 
-    // Message bhejne ka logic
     socket.on('chat message', (data) => {
-        io.emit('chat message', {
-            user: users[socket.id] ? users[socket.id].username : 'Unknown',
-            msg: data.msg,
-            avatar: users[socket.id] ? users[socket.id].avatar : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'
-        });
+        if (users[socket.id]) {
+            io.emit('chat message', { user: users[socket.id].username, msg: data.msg, avatar: users[socket.id].avatar });
+        }
     });
 
     socket.on('disconnect', () => {
-        if (users[socket.id]) {
-            console.log(users[socket.id].username + " left");
-            delete users[socket.id];
-            io.emit('updateUserList', Object.values(users));
-        }
+        delete users[socket.id];
+        io.emit('updateUserList', Object.values(users));
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
-});
+http.listen(PORT, () => { console.log('Server Live!'); });
